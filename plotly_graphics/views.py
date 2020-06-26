c63acdfbed85234f00ba1ce99fc666a4b03ebfc9
Karen <karen.chibuxchyan@marktguru.com>
2020-06-26 12:04:39 +0400

    1changed graphic to iris dataset

--- plotly_graphics/views.py
+++ plotly_graphics/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 
 def plot_graphic(request):
-    fig = px.scatter({'my_x': [0, 1, 2, 3, 4], 'my_y': [0, 1, 4, 9, 16]}, x='my_x', y='my_y', hover_name=['a', 'b', 'c', 'd', 'e'])
-
+    df = px.data.iris()
+    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+                     size='petal_length', hover_data=['petal_width'])
     graph = fig.to_html(full_html=False, default_height=500, default_width=700)
     context = {'graph': graph}
     return render(request, 'plotly_graphics/plot.html', context=context)
