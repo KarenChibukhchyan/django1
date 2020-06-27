c2ddd2034ed31a2822369389c60945d21b1afacd
Karen <karen.chibuxchyan@marktguru.com>
2020-06-27 22:57:10 +0400

    3D graphic added

--- plotly_graphics/views.py
+++ plotly_graphics/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 
 
 def plot_graphic(request):
-    df = px.data.iris()
-    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
-                     size='petal_length', hover_data=['petal_width'])
-    graph = fig.to_html(full_html=False, default_height=500, default_width=700)
+    df = px.data.election()
+    fig = px.scatter_3d(df, x="Joly", y="Coderre", z="Bergeron", color="winner", size="total", hover_name="district",
+                        symbol="result", color_discrete_map={"Joly": "blue", "Bergeron": "green", "Coderre": "red"})
+
+    graph = fig.to_html(full_html=False, default_height=900, default_width=900)
     context = {'graph': graph}
     return render(request, 'plotly_graphics/plot.html', context=context)
