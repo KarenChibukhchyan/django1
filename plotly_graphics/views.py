6f0de7180363330546cda08c3bdec451f1710f57
Karen <karen.chibuxchyan@marktguru.com>
2020-06-27 16:38:52 +0400

    Removed plotly numpy and pandas

--- plotly_graphics/views.py
+++ plotly_graphics/views.py
@@ -1,11 +1,11 @@
-import plotly.express as px
+# import plotly.express as px
 from django.shortcuts import render
 
 
 def plot_graphic(request):
-    df = px.data.iris()
-    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
-                     size='petal_length', hover_data=['petal_width'])
-    graph = fig.to_html(full_html=False, default_height=500, default_width=700)
-    context = {'graph': graph}
-    return render(request, 'plotly_graphics/plot.html', context=context)
+    # df = px.data.iris()
+    # fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+    #                  size='petal_length', hover_data=['petal_width'])
+    # graph = fig.to_html(full_html=False, default_height=500, default_width=700)
+    # context = {'graph': graph}
+    return render(request, 'plotly_graphics/plot.html', context='context')
